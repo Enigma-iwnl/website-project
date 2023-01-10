ef7fd3d100204c2521b526a961f1ebc0c51e6f70
Enigma-iwnl <108947845+Enigma-iwnl@users.noreply.github.com>
2023-01-10 14:04:21 -0500

    intial

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const port = 1337;
+
+app.get("/", (req, res) => {
+
+}
+)
+
+app.listen(port, ()=>{
+    console.log(`Listening on port: ${port}`);
+})
+ 
